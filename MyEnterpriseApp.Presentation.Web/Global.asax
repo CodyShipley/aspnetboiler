77187cfc1ee3e3cf58b8ad70e0cbde7f5c879ac5
CodyShipley <codybshipley@gmail.com>
2015-04-27 15:19:15 -0700

    Boiler

---
+++ MyEnterpriseApp.Presentation.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyEnterpriseApp.Presentation.Web.MvcApplication" Language="C#" %>
---
+++ MyEnterpriseApp.Presentation.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyEnterpriseApp.Presentation.Web.Startup))]
+namespace MyEnterpriseApp.Presentation.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
